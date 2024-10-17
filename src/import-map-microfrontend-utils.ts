4255f57e5e833eef984889c64797de5cae8002d0
Joel Denning <joeldenning@gmail.com>
2024-10-17 16:08:26 -0600

    Rewrite to ImportMapMicrofrontendUtils class (#2)
    * Rewrite to class
    
    * Documentation

--- src/import-map-microfrontend-utils.ts
+++ src/import-map-microfrontend-utils.ts
@@ -1,5 +1,36 @@
-export function microfrontendNameToFolderName(
-  microfrontendName: string,
-): string {
-  return microfrontendName.replace(/@/g, "").replace(/\//, "-");
+export class ImportMapMicrofrontendUtils {
+  declare baseOrigin: string;
+
+  constructor(init: Init) {
+    if (init.baseOrigin.endsWith("/")) {
+      throw Error(`baseOrigin must not end with slash`);
+    }
+
+    this.baseOrigin = init.baseOrigin;
+  }
+
+  getMicrofrontendURLPrefix(microfrontendName: string): string {
+    return (
+      this.baseOrigin +
+      `/${this.getMFEsFolderName()}/` +
+      microfrontendName.replace(/@/g, "").replace(/\//, "-") +
+      "/"
+    );
+  }
+
+  getDependenciesURLPrefix(): string {
+    return this.baseOrigin + `/${this.getDependenciesFolderName()}/`;
+  }
+
+  getDependenciesFolderName() {
+    return "deps";
+  }
+
+  getMFEsFolderName() {
+    return "mfes";
+  }
+}
+
+interface Init {
+  baseOrigin: string;
 }
